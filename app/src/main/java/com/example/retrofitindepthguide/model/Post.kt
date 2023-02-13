d8eff4223f47018e4c28f06497fb0ca7c1317d25
stefan <stefanbayne@gmail.com>
2023-02-13 14:53:41 -0500

    just more snippets.

--- app/src/main/java/com/example/retrofitindepthguide/model/Post.kt
+++ app/src/main/java/com/example/retrofitindepthguide/model/Post.kt
@@ -1,8 +1,10 @@
 package com.example.retrofitindepthguide.model
 
+import java.io.Serializable
+
 data class Post(
     val userID: Int,
     val id: Int,
     val title: String,
     val body: String
-)
+) : Serializable // allows for us to pass in this object as an object to UI
