c81bf9e5db1cece20ef14672caa9470e8cfd4e78
stefan bayne <stefanbayne@gmail.com>
2024-01-26 19:30:34 -0500

    adding operator methods.

--- app/src/main/java/com/example/retrofitindepthguide/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/retrofitindepthguide/viewmodel/DetailViewModel.kt
@@ -24,6 +24,7 @@ class DetailViewModel : ViewModel() {
     private val _post: MutableLiveData<Post> = MutableLiveData()
     val post: LiveData<Post>
         get() = _post
+    private operator fun MutableLiveData<Post>.invoke(post: Post) = _post.postValue(post)
 
     /*
     Variable to get the list of Post with backing property
@@ -31,6 +32,7 @@ class DetailViewModel : ViewModel() {
     private val _user: MutableLiveData<User> = MutableLiveData()
     val user: LiveData<User>
         get() = _user
+    private operator fun MutableLiveData<User>.invoke(user: User) = _user.postValue(user)
 
     /*
     Live Data value to note if loading list or not
@@ -38,6 +40,7 @@ class DetailViewModel : ViewModel() {
     private val _isLoading = MutableLiveData(false)
     val isLoading: LiveData<Boolean>
         get() = _isLoading
+    private operator fun MutableLiveData<Boolean>.invoke(state: Boolean) = _isLoading.postValue(state)
 
     /*
     Used in UI layer with Coroutines for asynchronous programming.
@@ -48,13 +51,13 @@ class DetailViewModel : ViewModel() {
 
         // Coroutine style
         viewModelScope.launch {
-            _isLoading.value = true
+            _isLoading(true)
             val fetchedPost = api.getPost(postId)
             val fetchedUser = api.getUser(fetchedPost.id)
             Log.i("CHECK", "User Id is ${fetchedPost.id}")
-            _post.value = fetchedPost
-            _user.value = fetchedUser
-            _isLoading.value = false
+            _post(fetchedPost)
+            _user(fetchedUser)
+            _isLoading(false)
         }
     }
 
@@ -63,33 +66,33 @@ class DetailViewModel : ViewModel() {
      */
     private fun fetchDataCallbackStyle(postId: Int) {
         val api = RetrofitInstance.api
-        _isLoading.value = true
+        _isLoading(true)
 
         api.getPostUsingCallbacks(postId).enqueue(object : Callback<Post> {
             override fun onFailure(call: Call<Post>, t: Throwable) {
                 Log.e(TAG, "Error code: $t")
-                _isLoading.value = false
+                _isLoading(false)
             }
 
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
                 if (response.isSuccessful) {
                     val fetchedPost = response.body()!!
-                    _post.value = fetchedPost
-                    _isLoading.value = false
+                    _post(fetchedPost)
+                    _isLoading(false)
 
                     // fetch post, then make second api call
                     api.getUserUsingCallbacks(fetchedPost.id).enqueue(object : Callback<User> {
 
                         override fun onFailure(call: Call<User>, t: Throwable) {
                             Log.e(TAG, "Error code: $t")
-                            _isLoading.value = false
+                            _isLoading(false)
                         }
 
                         override fun onResponse(call: Call<User>, response: Response<User>) {
                             if (response.isSuccessful) {
-                                _isLoading.value = false
+                                _isLoading(false)
                                 val fetchedUser = response.body()!!
-                                _user.value = fetchedUser
+                                _user(fetchedUser)
                             } // end if
                         }
                     })
